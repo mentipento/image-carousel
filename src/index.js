f5570eb512407a3a3f13a48688d7f5570c22e1bc
mentipento <26530260+mentipento@users.noreply.github.com>
2025-02-24 17:52:52 +0100

    add image carousel and previous/next buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 // index.js
 
 import './styles.css';
+
+
+const btnRight = document.querySelector('.carousel__button--right'); // Rechts-Klick-Button
+const btnLeft = document.querySelector('.carousel__button--left');
+const track = document.querySelector('.carousel__track'); // UL-Element, das alle Bilder enthält
+const slides = document.querySelectorAll('.carousel__slide'); // Alle Bilder
+
+btnRight.addEventListener('click', () => {
+    const firstSlide = track.firstElementChild; // Das erste Bild im Track
+    track.appendChild(firstSlide); // Bild ans Ende der Liste verschieben
+});
+
+btnLeft.addEventListener('click', () => {
+    const lastSlide = track.lastElementChild;
+    track.prepend(lastSlide);
+})
